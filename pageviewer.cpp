63cb0812df7dcd96a28b73e2f088519461e38718
JiriKalvoda <jirik.kalvoda@seznam.cz>
2019-05-22 16:29:41 +0200

    CODE Add button to PageViewer GUI (whit no function).

--- pageviewer.cpp
+++ pageviewer.cpp
@@ -8,36 +8,56 @@
 
 PageViewer::PageViewer(QWidget *parent) : QWidget(parent)
 {
-    sort.setSortRole(Qt::UserRole);
-    table.setModel(&sort);
+    table = new QTableView;
+    l = new QVBoxLayout;
+    buttons = new QHBoxLayout;
+    button_hide = new QPushButton;
+    button_delete = new QPushButton;
+    button_compare = new QPushButton;
+    buttons_middleLabel = new QLabel;
 
-    table.setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    table.setSizeAdjustPolicy(QAbstractScrollArea::AdjustToContents);
-    table.horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    sort = new QSortFilterProxyModel ;
+
+    sort->setSortRole(Qt::UserRole);
+    table->setModel(sort);
+
+    table->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    table->setSizeAdjustPolicy(QAbstractScrollArea::AdjustToContents);
+    table->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
     //table.setItemDelegate(0);
-    table.setSortingEnabled(1);
+    table->setSortingEnabled(1);
 
-    connect(&table,SIGNAL(pressed(QModelIndex)),this,SLOT(clicked(QModelIndex)));
+    connect(table,SIGNAL(pressed(QModelIndex)),this,SLOT(clicked(QModelIndex)));
 
-    l.addWidget(&table);
-    setLayout(&l);
+    button_delete->setText("Delete");
+    button_hide->setText("Hide");
+    button_compare->setText("Coprare difference");
+    buttons->addWidget(button_delete,0);
+    buttons->addWidget(button_hide,0);
+    buttons->addWidget(buttons_middleLabel,1);
+    buttons->addWidget(button_compare,0);
+
+
+    l->addWidget(table);
+    l->addLayout(buttons);
+    setLayout(l);
 
 }
 void PageViewer::setModel(QAbstractItemModel *model)
 {
-    sort.setSourceModel(model);
+    sort->setSourceModel(model);
 }
 void PageViewer::clicked(QModelIndex index)
 {
     qDebug ( ) << "clicked" << index.column() <<" "<< index.row();
     if(index.column()== 1)
     {
-        qDebug() << QUrl(QString("file:")+QDir::currentPath()+"/history/"+sort.data(index,Qt::DisplayRole).toString());
-        QDesktopServices::openUrl(QUrl(QString("file:")+QDir::currentPath()+"/history/"+sort.data(index,Qt::DisplayRole).toString()));
+        //qDebug() << QUrl(QString("file:")+QDir::currentPath()+"/history/"+sort->data(index,Qt::DisplayRole).toString());
+        QDesktopServices::openUrl(QUrl(QString("file:")+QDir::currentPath()+"/history/"+sort->data(index,Qt::DisplayRole).toString()));
     }
-    if(index.column()==sort.columnCount()-1)
+    if(index.column()==sort->columnCount()-1)
     {
-        sort.removeRows(index.row(),1);
+        sort->removeRows(index.row(),1);
     }
 
 }
--- pageviewer.h
+++ pageviewer.h
@@ -4,8 +4,11 @@
 #include <QWidget>
 #include <QTableView>
 #include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QPushButton>
 #include <QSortFilterProxyModel>
 #include <QIdentityProxyModel>
+#include <QLabel>
 
 
 class PageViewer : public QWidget
@@ -13,10 +16,13 @@ class PageViewer : public QWidget
     Q_OBJECT
 public:
     explicit PageViewer(QWidget *parent = 0);
-    QTableView table;
-    QVBoxLayout l;
+    QTableView *table;
+    QVBoxLayout *l;
+    QHBoxLayout *buttons;
+    QPushButton *button_hide, *button_delete, *button_compare;
+    QLabel * buttons_middleLabel;
     virtual void setModel(QAbstractItemModel *model);
-    QSortFilterProxyModel sort;
+    QSortFilterProxyModel *sort;
 signals:
 
 public slots:
