e99af138f6bc8a5fe165601b9424492879637bf0
JiriKalvoda <jirik.kalvoda@seznam.cz>
2020-01-24 21:36:35 +0100

    DEPLOY Change zipping in deploy file on linux.

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,16 @@ cqtdeployer -bin build-WebUpdatingIndicator-Desktop_Qt_5_12_5_GCC_64bit-Release/
 echo end cqtdeplyer | lolcat 
 cp Logo.ico DistributionKit
 cp pages.json DistributionKit
+if [ -n "$1" ];
+then
+	echo "make zip $1" | lolcat
+	cp -r DistributionKit $1
+	bname=$(basename "$1")
+	cd "$(dirname "$1")"
+	echo dirname $(dirname "$1") | lolcat
+	echo basename $bname | lolcat
+	rm $bname.zip
+	zip -9 $bname.zip -r $bname/*  
+	rm -r $bname
+fi
 
